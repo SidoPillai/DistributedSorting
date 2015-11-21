982a52a0912f6455513ccd60712fee45b36459e3
suhas.callin <suhas.callin@gmail.com>
2015-11-20 20:56:18 -0500

    added heapsort

---
+++ Distributed/src/Distributedpi.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Distributedpi {
+
+	public static void main(String args[]) throws IOException
+	{
+
+		File file = new File("/home/suhaspillai/javaprograms/TopicsDistributed/src/new_dataset_10000.txt");
+		FileInputStream f= new FileInputStream(file);
+		int SIZE = (int)file.length();
+		byte[] barray = new byte[SIZE];
+		FileChannel ch = f.getChannel();
+		MappedByteBuffer mb = ch.map(FileChannel.MapMode.READ_ONLY,0L, ch.size());
+		int i = 0;
+		while(mb.hasRemaining())
+		{
+			barray[i]=mb.get();
+			i++;	
+		}
+		ArrayList<String> valToSort = new ArrayList<String>(); 
+		String	str = new String (barray);
+		Scanner s = new Scanner(str);
+		while (s.hasNext())
+		{
+			valToSort.add(s.next());
+		}
+
+
+		Heap heap = new Heap(valToSort);
+		heap.HeapSort();
+		for(int i1 = 1 ; i1 < valToSort.size(); i1++)
+		{
+			System.out.println(valToSort.get(i1));
+		}
+
+	}
+
+}
---
+++ Distributed/src/Heap.java
@@ -0,0 +1,226 @@
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Heap {
+
+	ArrayList<String> alist;
+	Pattern pattern;
+	Matcher match;
+	public Heap(ArrayList<String> alist)
+	{
+		this.alist = alist;
+		pattern = Pattern.compile("[a-zA-Z]");
+
+	}
+
+	/* Get the Small value between the two string */
+	public String getSmallVal(String a , String b )
+	{
+		match = pattern.matcher(a);
+		int count =0;
+		while(match.find())
+		{
+			count++;
+		}
+		String sa_1 = a.substring(0,count);
+		String sa_2 = a.substring(count);
+
+		match = pattern.matcher(b);
+		count = 0;
+		while(match.find())
+		{
+			count++;
+		}
+		String sb_1 = b.substring(0,count);
+		String sb_2 = b.substring(count);
+
+		int sa_int = Integer.parseInt(sa_2);
+		int sb_int = Integer.parseInt(sb_2);
+
+		if(sa_1.compareTo(sb_1)<0)
+		{
+			return a;
+		}
+		else if (sa_1.compareTo(sb_1)>0)
+		{
+			return b;
+		}
+		else 		
+		{
+			if(sa_int<sb_int)
+			{
+				return a;
+			}
+			else{
+				return b;
+			}
+		}
+	}
+
+	/*Check if parent value is greater than the child node.*/
+	public boolean checkParentChild(String a, String b)
+	{
+
+		match = pattern.matcher(a);
+		int count =0;
+		while(match.find())
+		{
+			count++;
+		}
+		String sa_1 = a.substring(0,count);
+		String sa_2 = a.substring(count);
+
+		match = pattern.matcher(b);
+		count = 0;
+		while(match.find())
+		{
+			count++;
+		}
+		String sb_1 = b.substring(0,count);
+		String sb_2 = b.substring(count);
+
+		int sa_int = Integer.parseInt(sa_2);
+		int sb_int = Integer.parseInt(sb_2);
+
+
+		if(sa_1.compareTo(sb_1)<0)
+		{
+			return true;
+		}
+		else if (sa_1.compareTo(sb_1)>0)
+		{
+			return false;
+		}
+		else 		
+		{
+			if(sa_int<sb_int)
+			{
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+
+	}
+
+	/* This procedure adjusts the heap and gets the minimum element at the top*/
+	public void Heapify(int i , int length)
+
+	{
+
+		if(2*i > length && (2*i+1) > length )
+		{
+			return ;
+		}
+		else
+		{
+			String smallVal=null;
+			int leftindex = 2*i ;
+			int rightindex = 2*i + 1;
+			String leftVal;
+			String rightVal;
+			String middleVal;
+			String temp;
+			if(leftindex<=length && rightindex<=length)
+			{	
+				middleVal = alist.get(i);
+				leftVal = alist.get(leftindex);
+				rightVal = alist.get(rightindex); 
+				smallVal = getSmallVal(leftVal,rightVal);
+
+				if(!(checkParentChild(middleVal,smallVal)))
+				{
+					if(smallVal.equals(leftVal))
+					{
+						temp = alist.get(i);
+						alist.set(i, leftVal);
+						alist.set(leftindex,temp);
+						Heapify(leftindex,length);
+					}
+					else if(smallVal.equals(rightVal))
+					{
+						temp = alist.get(i);
+						alist.set(i,rightVal);
+						alist.set(rightindex,temp);
+						Heapify(rightindex,length);
+					}
+				}
+
+			}
+			else if(leftindex<=length)
+			{
+				middleVal = alist.get(i);
+				leftVal = alist.get(leftindex);
+				if(!(checkParentChild(middleVal,leftVal)))
+				{
+					temp = alist.get(i);
+					alist.set(i, leftVal);
+					alist.set(leftindex,temp);
+					Heapify(leftindex,length);
+				}
+			}
+
+			else if (rightindex <= length)
+			{
+				middleVal = alist.get(i);
+				rightVal = alist.get(rightindex);
+				if(!(checkParentChild(middleVal,rightVal)))
+				{
+					temp = alist.get(i);
+					alist.set(i,rightVal);
+					alist.set(rightindex,temp);
+					Heapify(rightindex,length);
+				}
+			}
+
+
+		}
+	}
+
+	/*To sort the heap*/
+	public ArrayList<String> HeapSort()
+	{
+
+
+		String temp=null;
+		String temp2 = null;
+		int counter=1;
+		int length = alist.size();
+
+		for (int i = length/2; i>=1; i--)
+		{
+			Heapify(i,length-1);
+		}
+		int index = 1;
+		counter = 1;
+		for(int i = 1; i < length-1 ; i++)
+		{
+			index = length-counter;
+			temp = alist.get(1);
+			temp2  = alist.get(index);
+			//alist.set(length-counter, temp);
+			alist.set(1,temp2);
+			alist.set(index,temp);
+			Heapify(1,length-(i+1)); // Deleting the end nodes.
+			counter++;
+
+		}
+		//Reverse the list now
+		String tempval;
+		counter = 1;
+		/* Reverse the array to get the sorted output*/
+		for (int j = alist.size()-1 ; j > length/2 ; j--)
+		{
+			tempval = alist.get(j);
+			alist.set(j,alist.get(counter));
+			alist.set(counter,tempval);
+			counter++;
+		}
+
+		return alist;
+	}
+
+}
